dca8fc1e77a0fa2c355d73f3a3d89c07189ec438
andreykarganov <karganov.an@yandex.ru>
2024-04-12 15:45:01 +0300

    seminar 2

--- app/urls.py
+++ app/urls.py
@@ -1,1 +1,1 @@
-from django.urls import pathfrom app import viewsurlpatterns = [    path('', views.index, name='index')]
+from django.urls import pathfrom app import viewsurlpatterns = [    path('', views.index, name='index'),    path('hot/', views.hot, name='hot'),    path('question/<int:question_id>', views.question, name= 'question')]
--- app/views.py
+++ app/views.py
@@ -1,12 +1,24 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 # Create your views here.
 QUESTIONS = [
     {
+        "id": i,
         "title" : f"Question {i}",
         "text" : f"Question text number {i}",
         "photo" : "../static/images/avatar.jpg"
-    } for i in range(10)
+    } for i in range(40)
 ]
 def index(request):
-    return render(request, "index.html", {"questions": QUESTIONS})
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(QUESTIONS, 5)
+    page_obj = paginator.page(page_num)
+    return render(request, "index.html", {"questions": page_obj})
+
+def hot(request):
+    return render(request, "hot.html", {"questions": QUESTIONS})
+
+def question(request, question_id):
+    item = QUESTIONS[question_id]
+    return render(request, "question.html", {"question": item})
