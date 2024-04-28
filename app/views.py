151972a35ee69b598136361f53bd923e2332a074
andreykarganov <karganov.an@yandex.ru>
2024-04-28 17:28:02 +0300

    finished nice pagination

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ TAGS = [
         "id": i,
         "name": f"tagName {i}",
         "posts": [i, i + 1],
-        "questions":  [1, 2, 5]
+        "questions":  [1, 2, 5, 8, 2, 12, 34, 37, 44, 71, 78, 90]
     } for i in range(40)
 ]
 QUESTIONS = [
@@ -21,19 +21,24 @@ QUESTIONS = [
     } for i in range(400)
 ]
 
+def paginate(objects_list, request, per_page=10):
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(objects_list, per_page)
+    try:
+        paginator.page(page_num)
+    except:
+        page_num = 1
+    return paginator.page(page_num)
+
 
 
 def index(request):
-    page_num = request.GET.get('page', 1)
-    paginator = Paginator(QUESTIONS, 5)
-    page_obj = paginator.page(page_num)
+    page_obj = paginate(QUESTIONS, request)
     popular_tags = [TAGS[tag_id] for tag_id in range (10)]
     return render(request, "index.html", {"questions": page_obj, "popular_tags":popular_tags, "tags": TAGS})
 
 def hot(request):
-    page_num = request.GET.get('page', 1)
-    paginator = Paginator(QUESTIONS, 5)
-    page_obj = paginator.page(page_num)
+    page_obj = paginate(QUESTIONS, request)
     popular_tags = [TAGS[tag_id] for tag_id in range(10)]
     return render(request, "hot.html", {"questions": page_obj, "popular_tags":popular_tags, "tags": TAGS})
 
@@ -55,7 +60,8 @@ def tag_page(request, tag_id):
     item = TAGS[tag_id]
     popular_tags = [TAGS[tag_id] for tag_id in range(10)]
     questions = [QUESTIONS[i] for i in item["questions"]]
-    return render(request, "tag-page.html", {"tag": item, "popular_tags": popular_tags, "tags": TAGS, "questions": questions})
+    page_obj = paginate(questions, request)
+    return render(request, "tag-page.html", {"tag": item, "popular_tags": popular_tags, "tags": TAGS, "questions": page_obj})
 
 def register(request):
     return render(request, "register.html")
