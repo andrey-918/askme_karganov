85cc06ab520a6dcfe29f4869988d73da8deda2a3
andreykarganov <karganov.an@yandex.ru>
2024-05-12 17:21:15 +0300

    db in settings.py

--- app/models.py
+++ app/models.py
@@ -9,11 +9,23 @@ class Profile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        app_label = 'profile'
+
+    def __str__(self):
+        return self.user
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        app_label = 'tag'
+
+    def __str__(self):
+        return self.name
 
 
 class QuestionManager(models.Manager):
@@ -33,6 +45,12 @@ class Question(models.Model):
 
     objects = QuestionManager()
 
+    class Meta:
+        app_label = 'question'
+
+    def __str__(self):
+        return self.title
+
 
 class Answer(models.Model):
     title = models.CharField(max_length=255)
@@ -42,22 +60,34 @@ class Answer(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        app_label = 'answer'
+
+    def __str__(self):
+        return self.title
 
 class AnswerLike(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     answer = models.ForeignKey(Answer, on_delete=models.PROTECT)
 
     class Meta:
+        app_label = 'answer_like'
         constraints = [
             models.UniqueConstraint(fields=['user', 'answer'], name='unique_answerlike_user')
         ]
 
+    def __str__(self):
+        return self.user
 
 class QuestionLike(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     question = models.ForeignKey(Question, on_delete=models.PROTECT)
 
     class Meta:
+        app_label = 'question_like'
         constraints = [
             models.UniqueConstraint(fields=['user', 'question'], name='unique_questionlike_user')
         ]
+
+    def __str__(self):
+        return self.user
--- askme_karganov/settings.py
+++ askme_karganov/settings.py
@@ -75,12 +75,23 @@ WSGI_APPLICATION = 'askme_karganov.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'askme_karganov_db',
+        'USER': 'postgres',
+        'PASSWORD': 'first911',
+        'HOST': 'localhost',
     }
 }
+# python3 manage.py migrate
 
 
 # Password validation
